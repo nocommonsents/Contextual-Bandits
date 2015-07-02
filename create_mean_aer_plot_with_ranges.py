c38373446b964da3fcd35b561c4d511297b2c087
Mike Bixler <bixlermike@gmail.com>
2015-07-02 11:45:00 -0400

    Range plot updated

--- create_mean_aer_plot_with_ranges.py
+++ create_mean_aer_plot_with_ranges.py
@@ -2,11 +2,14 @@ __author__ = 'bixlermike'
 
 #/usr/local/bin/python
 
+import matplotlib
+from matplotlib import rc
 from matplotlib.font_manager import FontProperties
 from matplotlib.ticker import MaxNLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
 import numpy as np
-
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
 majorFormatter = FormatStrFormatter('%d')
 
@@ -17,16 +20,16 @@ data3 = np.genfromtxt('banditMaxAERSummary.csv', delimiter=',', names = True)
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-ax.set_title("Relative Mean AER Range vs. Evaluation Number - BinomialUCI Algorithm")
-ax.set_xlabel('Number of Evaluations')
-ax.set_ylabel('Relative AER')
+ax.set_title(r"$Relative\ Mean\ AER\ Range\ vs.\ Evaluation\ Number\ -\ Binomial\ UCI\ Algorithm$", fontsize='16', y=1.02)
+ax.set_xlabel(r"$Number\ of\ Evaluations$")
+ax.set_ylabel(r"$Relative\ Mean\ AER$")
 
 minimum = data2['BinomialUCI']/data['Random']
 mean = data['BinomialUCI']/data['Random']
 maximum = data3['BinomialUCI']/data['Random']
-ax.plot(data['t'], maximum, label='Max')
-ax.plot(data['t'], mean, label='Mean')
-ax.plot(data['t'], minimum, label='Min')
+ax.plot(data['t'], maximum, label='Max', lw='1.25', color='green')
+ax.plot(data['t'], mean, label='Mean', lw='1.25', color='blue')
+ax.plot(data['t'], minimum, label='Min', lw='1.25', color='red')
 ax.fill_between(data['t'], minimum, maximum, facecolor='blue', alpha=0.5)
 
 
@@ -39,7 +42,8 @@ ax.legend(loc='lower right')
 #ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 ax.get_xaxis().set_major_locator(MaxNLocator(integer=True))
 ax.set_xticklabels(ax.xaxis.get_majorticklocs(), rotation=45)
-ax.set_xlim([0, min(max(data['t']),max(data2['t']),max(data3['t']))])
+#ax.set_xlim([0, min(max(data['t']),max(data2['t']),max(data3['t']))])
+ax.set_xlim([0,340000])
 ax.set_axisbelow(True)
 ax.xaxis.grid(color='gray', linestyle='dashed')
 ax.yaxis.grid(color='gray', linestyle='dashed')
@@ -49,6 +53,6 @@ ax.xaxis.set_major_formatter(majorFormatter)
 #    ax1.axhline(y=ymaj,ls='-')
 plt.tight_layout()
 #plt.show()
-plt.savefig("plots/averageAERwithRangesBinomialUCIAlgorithm.png", bbox_inches='tight')
+plt.savefig("plots/averageAERwithRangesBinomialUCIAlgorithm.png", dpi=240, bbox_inches='tight')
 
 

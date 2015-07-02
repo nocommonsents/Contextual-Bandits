4e309af3121e91ca5f2b4bc0533480077ae3bb12
Mike Bixler <bixlermike@gmail.com>
2015-07-02 00:14:37 -0400

    Reviving mistakenly deleted file

---
+++ create_mean_aer_plot.py
@@ -0,0 +1,52 @@
+__author__ = 'bixlermike'
+
+#/usr/local/bin/python
+import matplotlib
+from matplotlib import rc
+from matplotlib.font_manager import FontProperties
+from matplotlib.ticker import MaxNLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+import numpy as np
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
+majorFormatter = FormatStrFormatter('%d')
+
+data = np.genfromtxt('banditMeanAERSummary.csv', delimiter=',', names = True)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.set_title(r"$Contextual\ Algorithm\ Comparison\ -\ Relative\ Mean\ AER$", fontsize='16', y=1.02)
+ax.set_xlabel(r"$Number\ of\ Evaluations$")
+ax.set_ylabel(r"$Relative\ Mean\ AER$")
+
+ax.plot(data['t'],data['eGreedyContextual01']/data['Random'], lw='1.25', label=r'$eGreedy$', marker='o', markevery=500, fillstyle='none')
+ax.plot(data['t'],data['eAnnealingContextual']/data['Random'], lw='1.25', label=r'$eAnnealing$', marker='v', markevery=500, fillstyle='none')
+ax.plot(data['t'],data['SoftmaxContextual01']/data['Random'], lw='1.25', label=r'$Softmax(0.1)$', marker='^', markevery=500, fillstyle='none')
+ax.plot(data['t'],data['LinUCB01']/data['Random'], lw='1.25', label=r'$LinUCB$', marker='s', markevery=500, fillstyle='none')
+ax.plot(data['t'],data['NaiveBayesContextual']/data['Random'], lw='1.25', label=r'$NaiveBayes$', marker='*', markevery=500, fillstyle='none')
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+fontP = FontProperties()
+fontP.set_size('small')
+
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+#rc('text', usetex=True)
+
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop = fontP)
+ax.get_xaxis().set_major_locator(MaxNLocator(integer=True))
+ax.set_xticklabels(ax.xaxis.get_majorticklocs(), rotation=45)
+ax.set_xlim([0, max(data['t'])])
+ax.set_axisbelow(True)
+ax.xaxis.grid(color='gray', linestyle='dashed')
+ax.yaxis.grid(color='gray', linestyle='dashed')
+#ax.get_xaxis().set_major_locator(MaxNLocator(integer=True))
+ax.xaxis.set_major_formatter(majorFormatter)
+#for ymaj in ax1.yaxis.get_majorticklocs():
+#    ax1.axhline(y=ymaj,ls='-')
+#plt.tight_layout()
+
+plt.savefig("plots/averageAERContextual.png", dpi=240, bbox_inches='tight')
+
+

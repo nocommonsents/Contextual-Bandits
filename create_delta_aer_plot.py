741269766382a913c79d17b8f620f7e8991c0e7c
Mike Bixler <bixlermike@gmail.com>
2015-09-04 08:17:43 -0400

    Adding delta context-free back

---
+++ create_delta_aer_plot.py
@@ -0,0 +1,57 @@
+__author__ = 'bixlermike'
+
+#/usr/local/bin/python
+
+import matplotlib
+from matplotlib import rc
+from matplotlib.font_manager import FontProperties
+from matplotlib.ticker import MaxNLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+import numpy as np
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
+majorFormatter = FormatStrFormatter('%d')
+
+data = np.genfromtxt('banditMaxAERSummary.csv', delimiter=',', names = True)
+data2 = np.genfromtxt('banditMeanAERSummary.csv', delimiter=',', names = True)
+data3 = np.genfromtxt('banditMinAERSummary.csv', delimiter=',', names = True)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.set_title(r"$Context-Free\ Algorithm\ Comparison\ -\ Delta\ Relative\ AER$", fontsize='16', y=1.02)
+ax.set_xlabel(r"$Number\ of\ Evaluations$")
+ax.set_ylabel(r"$Delta\ Relative\ AER$")
+
+#ax.plot(data['t'],data['Random'], label='Random')
+ax.plot(data['NumEvals'],(data['MostClicked']-data3['MostClicked'])/data2['Random'], label=r'$MostClicked$', lw='1.25', marker='o', markevery=500, fillstyle='none')
+ax.plot(data['NumEvals'],(data['MostRecent']-data3['MostRecent'])/data2['Random'], label=r'$MostRecent$', lw='1.25', marker='v', markevery=500, fillstyle='none')
+ax.plot(data['NumEvals'],(data['MostCTR']-data3['MostCTR'])/data2['Random'], label=r'$HighestCTR$', lw='1.25', marker='^', markevery=500, fillstyle='none')
+ax.plot(data['NumEvals'],(data['eGreedy01']-data3['eGreedy01'])/data2['Random'], label=r'$e-Greedy(0.1)$', lw='1.25', marker='s', markevery=500, fillstyle='none')
+ax.plot(data['NumEvals'],(data['eAnnealing']-data3['eAnnealing'])/data2['Random'], label=r'$e-Annealing$', lw='1.25', marker='*', markevery=500, fillstyle='none')
+ax.plot(data['NumEvals'],(data['Softmax001']-data3['Softmax001'])/data2['Random'], label=r'$Softmax(0.01)$', lw='1.25', color='deepskyblue', marker='>', markevery=500, fillstyle='none')
+ax.plot(data['NumEvals'],(data['Softmax01']-data3['Softmax01'])/data2['Random'], label=r'$Softmax(0.1)$', lw='1.25', marker='+', markevery=500, fillstyle='none')
+ax.plot(data['NumEvals'],(data['EXP305']-data3['EXP305'])/data2['Random'], label=r'$EXP\ 3(0.5)$', color='fuchsia', lw='1.25', marker='D', markevery=500, fillstyle='none')
+ax.plot(data['NumEvals'],(data['UCB1']-data3['UCB1'])/data2['Random'], label=r'$UCB1$', color='lawngreen', lw='1.25', marker='<', markevery=500, fillstyle='none')
+ax.plot(data['NumEvals'],(data['BinomialUCI']-data3['BinomialUCI'])/data2['Random'], label=r'$BinomialUCI$', color='darkorange', lw='1.25', marker='x', markevery=500, fillstyle='none')
+
+
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+fontP = FontProperties()
+fontP.set_size('small')
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop = fontP)
+ax.get_xaxis().set_major_locator(MaxNLocator(integer=True))
+ax.set_xticklabels(ax.xaxis.get_majorticklocs(), rotation=45)
+ax.set_xlim([0, max(data['NumEvals'])])
+ax.xaxis.grid(color='gray', linestyle='dashed')
+ax.yaxis.grid(color='gray', linestyle='dashed')
+#ax.get_xaxis().set_major_locator(MaxNLocator(integer=True))
+ax.xaxis.set_major_formatter(majorFormatter)
+#for ymaj in ax1.yaxis.get_majorticklocs():
+#    ax1.axhline(y=ymaj,ls='-')
+
+plt.savefig("plots/deltaAER.png", dpi=240, bbox_inches='tight')
+
+
